e4460564084b0de1b21c5be9ed8e5294b3f7c8e2
Spencer Shaw <sfshaw@calpoly.edu>
2019-04-09 13:17:41 -0700

    Beautify code

--- delay.c
+++ delay.c
@@ -1,20 +1,23 @@
 #include "delay.h"
 
+// Initialize DCO and other clock values
 void init_dco(void){
     CS->KEY = CS_KEY_VAL;  // Unlock key
     CS->CTL0 = 0;  // Reset
     CS->CTL0 = CS_CTL0_DCORSEL_3;
-    CS->CTL1 = CS_CTL1_SELA_2 | // Set ACLK to REFCLK
-               CS_CTL1_SELS_3 | // Set SMCLK to DCO
+    CS->CTL1 = CS_CTL1_SELA_2 |  // Set ACLK to REFCLK
+               CS_CTL1_SELS_3 |  // Set SMCLK to DCO
                CS_CTL1_SELM_3;  // Set MCLK to DCO
-    CS->KEY = LOCK_CS_KEY; // Lock key
+    CS->KEY = LOCK_CS_KEY;  // Lock key
 }
 
-void safe_48(){
+// Apply safe settings for 48MHz operation
+static inline void safe_48(){
     /* Transition to VCORE Level 1: AM0_LDO --> AM1_LDO */
     while ((PCM->CTL1 & PCM_CTL1_PMR_BUSY));
-     PCM->CTL0 = PCM_CTL0_KEY_VAL | PCM_CTL0_AMR_1;
+    PCM->CTL0 = PCM_CTL0_KEY_VAL | PCM_CTL0_AMR_1;
     while ((PCM->CTL1 & PCM_CTL1_PMR_BUSY));
+
     /* Configure Flash wait-state to 1 for both banks 0 & 1 */
     FLCTL->BANK0_RDCTL = (FLCTL->BANK0_RDCTL &
      ~(FLCTL_BANK0_RDCTL_WAIT_MASK)) | FLCTL_BANK0_RDCTL_WAIT_1;
@@ -22,9 +25,10 @@ void safe_48(){
      ~(FLCTL_BANK1_RDCTL_WAIT_MASK)) | FLCTL_BANK1_RDCTL_WAIT_1;
 }
 
+// Set DCO to provide frequency
 void set_dco(unsigned int freq){
-    CS->KEY = CS_KEY_VAL;
-    CS->CTL0 = 0;
+    CS->KEY = CS_KEY_VAL;  // Unlock key
+    CS->CTL0 = 0;  // Reset
     switch(freq){
     case(FREQ_1_5_MHZ):
         CS->CTL0 = CS_CTL0_DCORSEL_0;
@@ -42,7 +46,7 @@ void set_dco(unsigned int freq){
         CS->CTL0 = CS_CTL0_DCORSEL_4;
         break;
     case(FREQ_48_MHZ):
-        safe_48();
+        safe_48();  // Apply settings for safe 48MHz operation
         CS->CTL0 = CS_CTL0_DCORSEL_5;
         break;
     default:
@@ -52,35 +56,59 @@ void set_dco(unsigned int freq){
     CS->KEY = 0;
 }
 
+// Loop counts for 1ms dealy at given frequencies
+// Includes fine tuned inline asm NOPs
 static inline int one_msec(const unsigned int freq){
     switch(freq){
     case(FREQ_48_MHZ):
-        return 3000;
+        return 6019;
     case(FREQ_24_MHZ):
-        return 1711;
+        NOP
+        NOP
+        NOP
+        return 3435;
     case(FREQ_12_MHZ):
-        return 852;
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        return 1714;
     case(FREQ_6_MHZ):
-        return 421;
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        return 853;
     case(FREQ_3_MHZ):
-        return 207;
+        NOP
+        NOP
+        NOP
+        return 423;
     default:
         // Default to 1.5MHz
-        return 98;
+        return 181;
     }
 }
 
+// Delay for 1ms at the given frequency
 inline delay_one_ms(const unsigned int freq){
      unsigned int j = one_msec(freq);
      while(j-- > 0);
 }
 
+// Delay for the given number of ms at the given frequency
 inline void delay_ms(unsigned int msec, const unsigned int freq){
     while (msec-- > 0){
         delay_one_ms(freq);
     }
 }
 
+// Fine tuned 1 us delay only to be used at 48MHz
+// Not claibrated for other frequencies
 inline void delay_one_us_at_48(){
     NOP
     NOP
@@ -99,15 +127,15 @@ inline void delay_one_us_at_48(){
     NOP
     NOP
     NOP
+    NOP
+    NOP
+    NOP
+    NOP
 }
 
+// Delay for the given number of us 
 inline void delay_us(unsigned int usec, const unsigned int freq){
     while(usec-- > 0){
-        delay_one_us_at_48()
+        delay_one_us_at_48();
     }
-}
-
-void delay_for_loop(const int iterations){
-    volatile int i;
-    for(i = iterations; i> 0; i--);
 }
--- led.c
+++ led.c
@@ -3,36 +3,29 @@
 
 // Setup LED1
 inline void init_led1(void){
-    P1->SEL0 &= ~BIT_0;
-    P1->SEL1 &= ~BIT_0;
-    P1->DIR |= GPIO_1_MODE_OUTPUT;
-    P1->OUT &= ~LED1_STATE_ON;
+    P1->SEL0 &= ~BIT_0;  // Set sel0 bit low for GPIO
+    P1->SEL1 &= ~BIT_0;  // Set sel1 bit low for GPIO
+    P1->DIR |= GPIO_1_MODE_OUTPUT;  // Set P1.0 to output mode 
+    P1->OUT &= ~LED1_STATE_ON;  //  Set LED1 state to off
 }
 
 // Toggle LED1
 inline void toggle_led1(void){
-    P1->OUT ^= LED1_STATE_ON;
-}
-
-void blink_led1(int msec, int freq){
-    toggle_led1();
-    delay_ms(msec, freq);
-    toggle_led1();
+    P1->OUT ^= LED1_STATE_ON;  // XOR LED1 state to toggle
 }
 
 
 // Setup RGB LED
 void init_led_rgb(void){
-    P1->SEL0 &= ~PIN_RGB_SEL0_GPIO;
-    P1->SEL1 &= ~PIN_RGB_SEL1_GPIO;
-    P2->DIR |= GPIO_RGB_MODE_OUTPUT;
-    P2->OUT &= ~LED_RGB_STATE_ON;
+    P1->SEL0 &= ~PIN_RGB_SEL0_GPIO;  // Set sel0 bits low for GPIO
+    P1->SEL1 &= ~PIN_RGB_SEL1_GPIO;  // Set sel1 bits low for GPIO
+    P2->DIR |= GPIO_RGB_MODE_OUTPUT;  // Set P2.0 - P2.2 to output mode
+    P2->OUT &= ~LED_RGB_STATE_ON;  // Set RGB LED state to off
 }
 
 // Set 3 bits of RGB LED
 void set_led_rgb_value(int value){
-    P2->OUT &= ~LED_RGB_STATE_ON;
-    P2->OUT |= (value & LED_RGB_STATE_ON);
+    P2->OUT &= ~LED_RGB_STATE_ON;  // Clear previous RGB LED state
+    P2->OUT |= (value & LED_RGB_STATE_ON);  // Set RGB LED state
 }
-
 
